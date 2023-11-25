46426be97377383fc3d2bbc416dd679e9b5c343d
HayvenX <pg766282@gmail.com>
2023-11-25 12:06:19 +0200

    Add all

---
+++ script.js
@@ -0,0 +1,22 @@
+let photo = document.getElementById("photo")
+
+let photoArray =
+[
+    "img/dog.jpg",
+    "img/pan.jpg",
+    "img/symbol.jpg",
+    "img/dice.jpg",
+    "img/rice.jpg"
+]
+
+function RandomNumber(max)
+{
+    return Math.floor(Math.random() * max)
+}
+
+function RandomPhoto()
+{
+    RandomNumber(5)
+    photo.setAttribute("src", photoArray[RandomNumber(5)])
+}
+setInterval(RandomPhoto, 2500)
